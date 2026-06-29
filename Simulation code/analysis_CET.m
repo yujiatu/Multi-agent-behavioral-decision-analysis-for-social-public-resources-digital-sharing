91eaacafcc381f9327674cf641ae1b8cfaaa5ddc
FanJiang <jiang_fan1220@163.com>
2026-06-29 23:53:29 +0800

    Update analysis_CET.m

--- Simulation code/analysis_CET.m
+++ Simulation code/analysis_CET.m
@@ -26,13 +26,11 @@ plot(t,y5(:,1),'-m','linewidth',1.5);hold on;
 plot(t,y1(:,1),'--r','linewidth',1.5);hold on;
 [t,y2]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.012,lambda),[0 0.5],[0.4 0.35 0.45]); 
 plot(t,y2(:,1),'--g','linewidth',1.5);hold on;
-% [t,y3]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,71.905,62.31,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
-% plot(t,y3(:,1),'--b','linewidth',1.5);hold on;
 [t,y4]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.06,lambda),[0 0.5],[0.4 0.35 0.45]); 
 plot(t,y4(:,1),'--k','linewidth',1.5);hold on;
 [t,y5]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.12,lambda),[0 0.5],[0.4 0.35 0.45]); 
 plot(t,y5(:,1),'--m','linewidth',1.5);hold on;
-xlabel({'(a)'}),ylabel('\itx'),%ylim([0 1])
+xlabel({'(a)'}),ylabel('\itx')
 set(gca, 'FontName', 'Times New Roman','FontSize',12)
 legend('\it\theta_s=0.001, \theta_d=0.036', '\it\theta_s=0.01, \theta_d=0.036', '\it\theta_s=0.03, \theta_d=0.036', ...
     '\it\theta_s=0.05, \theta_d=0.036', '\it\theta_s=0.10, \theta_d=0.036', ...
@@ -57,13 +55,11 @@ plot(t,y5(:,2),'-m','linewidth',1.5);hold on;
 plot(t,y1(:,2),'--r','linewidth',1.5);hold on;
 [t,y2]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.012,lambda),[0 2],[0.4 0.35 0.45]); 
 plot(t,y2(:,2),'--g','linewidth',1.5);hold on;
-% [t,y3]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,71.905,62.31,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
-% plot(t,y3(:,1),'--b','linewidth',1.5);hold on;
 [t,y4]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.06,lambda),[0 2],[0.4 0.35 0.45]); 
 plot(t,y4(:,2),'--k','linewidth',1.5);hold on;
 [t,y5]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.12,lambda),[0 2],[0.4 0.35 0.45]); 
 plot(t,y5(:,2),'--m','linewidth',1.5);hold on;
-xlabel({'(b)'}),ylabel('\ity'),%ylim([0 1])
+xlabel({'(b)'}),ylabel('\ity')
 set(gca, 'FontName', 'Times New Roman','FontSize',12)
 legend('\it\theta_s=0.001, \theta_d=0.036', '\it\theta_s=0.01, \theta_d=0.036', '\it\theta_s=0.03, \theta_d=0.036', ...
     '\it\theta_s=0.05, \theta_d=0.036', '\it\theta_s=0.10, \theta_d=0.036', ...
@@ -88,13 +84,11 @@ plot(t,y5(:,3),'-m','linewidth',1.5);hold on;
 plot(t,y1(:,3),'--r','linewidth',1.5);hold on;
 [t,y2]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.012,lambda),[0 2],[0.4 0.35 0.45]); 
 plot(t,y2(:,3),'--g','linewidth',1.5);hold on;
-% [t,y3]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,71.905,62.31,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
-% plot(t,y3(:,1),'--b','linewidth',1.5);hold on;
 [t,y4]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.06,lambda),[0 2],[0.4 0.35 0.45]); 
 plot(t,y4(:,3),'--k','linewidth',1.5);hold on;
 [t,y5]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,0.03,0.12,lambda),[0 2],[0.4 0.35 0.45]); 
 plot(t,y5(:,3),'--m','linewidth',1.5);hold on;
-xlabel({'(c)'}),ylabel('\itz'),%ylim([0 1])
+xlabel({'(c)'}),ylabel('\itz')
 set(gca, 'FontName', 'Times New Roman','FontSize',12)
 legend('\it\theta_s=0.001, \theta_d=0.036', '\it\theta_s=0.01, \theta_d=0.036', '\it\theta_s=0.03, \theta_d=0.036', ...
     '\it\theta_s=0.05, \theta_d=0.036', '\it\theta_s=0.10, \theta_d=0.036', ...
@@ -120,8 +114,6 @@ plot(t,y5(:,1),'-m','linewidth',1.5);hold on;
 plot(t,y1(:,1),'--r','linewidth',1.5);hold on;
 [t,y2]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,3.365,W,E2,E3,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
 plot(t,y2(:,1),'--g','linewidth',1.5);hold on;
-% [t,y3]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,71.905,62.31,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
-% plot(t,y3(:,1),'--b','linewidth',1.5);hold on;
 [t,y4]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,4.711,W,E2,E3,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
 plot(t,y4(:,1),'--k','linewidth',1.5);hold on;
 [t,y5]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,8.749,W,E2,E3,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
@@ -151,8 +143,6 @@ plot(t,y5(:,2),'-m','linewidth',1.5);hold on;
 plot(t,y1(:,2),'--r','linewidth',1.5);hold on;
 [t,y2]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,3.365,W,E2,E3,T2,T3,lambda),[0 5],[0.4 0.35 0.45]); 
 plot(t,y2(:,2),'--g','linewidth',1.5);hold on;
-% [t,y3]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,71.905,62.31,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
-% plot(t,y3(:,1),'--b','linewidth',1.5);hold on;
 [t,y4]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,4.711,W,E2,E3,T2,T3,lambda),[0 5],[0.4 0.35 0.45]); 
 plot(t,y4(:,2),'--k','linewidth',1.5);hold on;
 [t,y5]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,8.749,W,E2,E3,T2,T3,lambda),[0 5],[0.4 0.35 0.45]); 
@@ -182,8 +172,6 @@ plot(t,y5(:,3),'-m','linewidth',1.5);hold on;
 plot(t,y1(:,3),'--r','linewidth',1.5);hold on;
 [t,y2]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,3.365,W,E2,E3,T2,T3,lambda),[0 5],[0.4 0.35 0.45]); 
 plot(t,y2(:,3),'--g','linewidth',1.5);hold on;
-% [t,y3]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,71.905,62.31,T2,T3,lambda),[0 0.5],[0.4 0.35 0.45]); 
-% plot(t,y3(:,1),'--b','linewidth',1.5);hold on;
 [t,y4]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,4.711,W,E2,E3,T2,T3,lambda),[0 5],[0.4 0.35 0.45]); 
 plot(t,y4(:,3),'--k','linewidth',1.5);hold on;
 [t,y5]=ode45(@(t,y) func(t,y,M,B,p,L,C1,6.472,8.749,W,E2,E3,T2,T3,lambda),[0 5],[0.4 0.35 0.45]); 
