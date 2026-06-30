5477b7f4e6bc03296fb7a69d6929e9d238b3c31c
FanJiang <jiang_fan1220@163.com>
2026-06-30 18:19:29 +0800

    Update initial_state.m

--- Simulation code/initial_state.m
+++ Simulation code/initial_state.m
@@ -1,6 +1,6 @@
 clc,clear;close all;
 
-figure(1);
+figure(2);
 tiledlayout(2,2,'TileSpacing','tight');
 M=0.5;      B=3;     p=0.65;    L=70.34;    C1=48.55;
 E2=71.905;  T2=0.03;  C2=6.4715;
@@ -79,16 +79,21 @@ xlabel({'Time';'(c)'}),ylabel('\itz')
 set(gca, 'FontName', 'Times New Roman','FontSize',12)
 
 nexttile %xiangweitu
-for i=0.1:0.2:0.9
-    for j=0.1:0.2:0.9
-        for k=0.1:0.2:0.9  
-       [t,y]=ode45(@(t,y) func(t,y,M,B,L,p,C1,C2,C3,W,E2,E3,T2,T3,lambda),[0 1],[i j k]);               
-        plot3(y(:,1), y(:,2), y(:,3), 'linewidth',1.5);hold on
+for i=0.1:0.2:1
+    for j=0.1:0.2:1
+        for k=0.1:0.2:1   
+        [t,y]=ode45(@(t,y) func(t,y,M,B,p,L,C1,C2,C3,W,E2,E3,T2,T3,lambda),[0 10],[i j k]);               
+        plot3(y(:,1),y(:,2),y(:,3),'linewidth',1.5);
+        set(gca,'XTick',[0:0.2:1],'YTick',[0:0.2:1],'ZTick',[0:0.2:1])
+        hold on
+        axis([0 1 0 1 0 1])
+        view([45 10])
         end
     end
 end
 grid on
+hold on
 set(gca, 'FontName','Times New Roman','FontSize',12);
-xlabel({'\itx'}); ylabel({('\ity');('                                \rm(d)')}); zlabel('\itz');
-% set(gca,'XTick',[0:0.2:1],'YTick',[0:0.2:1],'ZTick',[0:0.2:1])
-xlim([0 1]),ylim([0 1]), zlim([0 1])
+xlabel({('\itx');('                                                             \rm(d)')},'Rotation',0);
+ylabel(('\ity'),'Rotation',0);
+zlabel('\itz','Rotation',360,'position',[0 0 1.05]);
